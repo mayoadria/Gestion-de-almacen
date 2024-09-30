c7effe038c73f97085fea68bf5107466c285baba
Christian <cgonzalez@alumnat.copernic.cat>
2024-09-30 13:17:57 +0200

    Commit inicial

---
+++ src/main/java/aplicacio/NewClass.java
@@ -0,0 +1,14 @@
+package aplicacio;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author chris
+ */
+public class NewClass {
+    
+}
