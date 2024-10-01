6514203df2ded9ba05a7571097b95c41e7a9d2ff
Anna <aserrano@alumnat.copernic.cat>
2024-10-01 09:31:08 +0200

    Classe Proveidor creada amb getters i setters. I he començat la ProveidorDAO.

---
+++ src/main/java/aplicacio/model/Proveidor.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplicacio.model;
+
+/**
+ *
+ * @author Anna
+ */
+public class Proveidor {
+    
+    String nom;
+    String cif;
+    EstatProveidor Estat;
+
+    public Proveidor(String nom, String cif, EstatProveidor Estat) {
+        this.nom = nom;
+        this.cif = cif;
+        this.Estat = Estat;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public EstatProveidor getEstat() {
+        return Estat;
+    }
+
+    public void setEstat(EstatProveidor Estat) {
+        this.Estat = Estat;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveidor{" + "nom=" + nom + ", cif=" + cif + ", Estat=" + Estat + '}';
+    }
+    
+    
+}
+
+
+public enum Estat{
+    ACTIU,INACTIU
+}
---
+++ src/main/java/dades/ProveidorDAO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dades;
+
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author Anna
+ */
+public class ProveidorDAO extends DataLayer implements DAOInterface<Proveidor>{
+    
+    public ProveidorDAO() throws SQLException() {
+        //Aquest mètode recull els errors de connexió que es puguin donar en la connexió amb la BBDD.
+        
+        super();
+    }
+}
