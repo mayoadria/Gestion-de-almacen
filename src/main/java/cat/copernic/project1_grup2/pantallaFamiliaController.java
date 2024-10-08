a6d011b20d402c63d8ef98cd32914de4b08e715a
orsaca00@gmail.com <=salvador.carretero.oriol@alumnat.copernic.cat>
2024-10-08 09:58:31 +0200

    commit for pull

--- src/main/java/cat/copernic/project1_grup2/pantallaFamiliaController.java
+++ src/main/java/cat/copernic/project1_grup2/pantallaFamiliaController.java
@@ -11,24 +11,28 @@ package cat.copernic.project1_grup2;
 
 import aplicacio.model.Familia;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class pantallaFamiliaController implements Initializable{
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }  
+   
     
     
     @FXML
@@ -50,25 +54,25 @@ public class pantallaFamiliaController implements Initializable{
     private Label lb_observacions;
 
     @FXML
-    private TableColumn<?, ?> tc_dataAlta;
+    private TableColumn<Familia, Date> tc_dataAlta;
 
     @FXML
-    private TableColumn<?, ?> tc_descripcio;
+    private TableColumn<Familia, String> tc_descripcio;
 
     @FXML
-    private TableColumn<?, ?> tc_id;
+    private TableColumn<Familia, Integer> tc_id;
 
     @FXML
-    private TableColumn<?, ?> tc_idProveidor;
+    private TableColumn<Familia, Integer> tc_idProveidor;
 
     @FXML
-    private TableColumn<?, ?> tc_nom;
+    private TableColumn<Familia, String> tc_nom;
 
     @FXML
-    private TableColumn<?, ?> tc_observacions;
+    private TableColumn<Familia, String> tc_observacions;
 
     @FXML
-    private TableView<?> tv_familia;
+    private TableView<Familia> tv_familia;
 
     @FXML
     private TextArea txt_areaDescripcio;
@@ -88,25 +92,161 @@ public class pantallaFamiliaController implements Initializable{
     @FXML
     private TextField txt_nom;
     
+    private ObservableList<Familia> families;
+
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    
+        families =  FXCollections.observableArrayList();
+        this.tc_id.setCellValueFactory(new PropertyValueFactory("id_fam"));
+        this.tc_nom.setCellValueFactory(new PropertyValueFactory("nom_familia"));
+        this.tc_descripcio.setCellValueFactory(new PropertyValueFactory("descripcio"));
+        this.tc_dataAlta.setCellValueFactory(new PropertyValueFactory("data_alta_fam"));
+        this.tc_idProveidor.setCellValueFactory(new PropertyValueFactory("id_proveidor_fam"));
+        this.tc_observacions.setCellValueFactory(new PropertyValueFactory("Observacions"));
+        
+    }
+    /* private int id_fam;
+    private String nom_familia;
+    private String descripcio;
+    private Date data_alta_fam;
+    private int id_proveidor_fam;
+    private String Observacions;*/
     
     @FXML
-    void Afegir(ActionEvent event) {
-
+    void afegirFamilia(ActionEvent event) {
+        
+        try{
+            
+        
+        int id = Integer.parseInt(this.txt_id.getText());
+        String nom = this.txt_nom.getText();
+        String descripcio = this.txt_areaDescripcio.getText();
+        //todo mirar tipus data sql.date apunts
+        String dataAltaText = this.txt_dataAlta.getText(); // Texto introducido por el usuario
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataAlta = LocalDate.parse(dataAltaText, formatter);
+        java.sql.Date sqlDate = java.sql.Date.valueOf(dataAlta);
+        int id_proveidor = Integer.parseInt(this.txt_idProveidor.getText());
+        String observacions = this.txt_areaObservacions.getText();
+        
+        Familia f = new Familia(id, nom, descripcio, sqlDate, id_proveidor, observacions);
+        
+        if (!this.families.contains(f)) {
+            this.families.add(f);
+            this.tv_familia.setItems(families);
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("la familia existeix");
+            alert.showAndWait();
+        }
+        }catch(NumberFormatException e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("la familia existeix");
+            alert.showAndWait();
+        }
     }
+        @FXML
+    void seleccionar(ActionEvent event) {
+        
+        Familia f = this.tv_familia.getSelectionModel().getSelectedItem(); //torna la entitat seleccionada dela taula
+        
+            if (f != null) 
+            {
+                
+                this.txt_id.setText(f.getId_fam() + "");
+                this.txt_nom.setText(f.getNom_familia());
+                this.txt_areaDescripcio.setText(f.getDescripcio());
+                this.txt_dataAlta.setText(f.getData_alta_fam() + "");
+                this.txt_idProveidor.setText(f.getId_proveidor_fam() + "");
+                this.txt_areaObservacions.setText(f.getObservacions());
+            }
+        
+   }
 
     @FXML
-    void Eliminar(ActionEvent event) {
+    void eliminar(ActionEvent event) {
 
     }
    
     @FXML
-    void Modificar(ActionEvent event) {
+    void modificar(ActionEvent event) {
+        Familia f = this.tv_familia.getSelectionModel().getSelectedItem(); //torna la entitat seleccionada dela taula
+        
+            if (f == null) 
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setTitle("Error");
+                alert.setContentText("has de seleccionar una familia");
+                alert.showAndWait();
+                
+            }else{
+                
+                try{
+                
+                int id = Integer.parseInt(this.txt_id.getText());
+                String nom = this.txt_nom.getText();
+                String descripcio = this.txt_areaDescripcio.getText();
+                //todo mirar tipus data sql.date apunts
+                String dataAltaText = this.txt_dataAlta.getText(); // Texto introducido por el usuario
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                LocalDate dataAlta = LocalDate.parse(dataAltaText, formatter);
+                java.sql.Date sqlDate = java.sql.Date.valueOf(dataAlta);
+                int id_proveidor = Integer.parseInt(this.txt_idProveidor.getText());
+                String observacions = this.txt_areaObservacions.getText();
 
+                Familia aux = new Familia(id, nom, descripcio, sqlDate, id_proveidor, observacions);
+
+                if (!this.families.contains(aux)) {
+                    
+                    f.setId_fam(aux.getId_fam());
+                    f.setNom_familia(aux.getNom_familia());
+                    f.setDescripcio(aux.getDescripcio());
+                    f.setData_alta_fam(aux.getData_alta_fam());
+                    f.setId_proveidor_fam(aux.getId_proveidor_fam());
+                    f.setObservacions(aux.getObservacions());
+                    
+                    this.tv_familia.refresh();
+                    
+                }else{
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setHeaderText(null);
+                    alert.setTitle("Error");
+                    alert.setContentText("la familia existeix");
+                    alert.showAndWait();
+                }
+                }catch(NumberFormatException e){
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setHeaderText(null);
+                    alert.setTitle("Error");
+                    alert.setContentText("la familia existeix");
+                    alert.showAndWait();
+                }
+            }
     }
 
     @FXML
-    void Sortir(ActionEvent event) {
-
+    void sortir(ActionEvent event) {
+        
+        Familia f = this.tv_familia.getSelectionModel().getSelectedItem(); //torna la entitat seleccionada dela taula
+        
+            if (f == null) 
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setTitle("Error");
+                alert.setContentText("has de seleccionar una familia");
+                alert.showAndWait();
+                
+            }else{
+                this.families.remove(f);
+                this.tv_familia.refresh();
+            }
     }
 
 }
--- src/main/java/dades/FamiliaDAO.java
+++ src/main/java/dades/FamiliaDAO.java
@@ -6,6 +6,9 @@ package dades;
 
 import dao.DAOInterface;
 import aplicacio.model.Familia;
+import dao.MyDataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -26,6 +29,22 @@ public class FamiliaDAO implements DAOInterface<Familia>{
 
     @Override
     public void insert(Familia t) throws SQLException {
+        String query = "INSERT INTO familia (id_fam, nom_familia, descripcio, data_alta_fam, id_proveidor_fam, Observacions) VALUES (?, ?, ?, ?, ?, ?)";
+    
+ 
+        
+        try (Connection conn = MyDataSource.getConnection();
+         PreparedStatement stmt = conn.prepareStatement(query)) {
+        
+        stmt.setInt(1, t.getId_fam());
+        stmt.setString(2, t.getNom_familia());
+        stmt.setString(3, t.getDescripcio());
+        stmt.setDate(4, t.getData_alta_fam());
+        stmt.setInt(5, t.getId_proveidor_fam());
+        stmt.setString(6, t.getObservacions());
+        
+        stmt.executeUpdate();
+    }
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
