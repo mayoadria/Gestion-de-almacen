3e7cae9b0d14a9cbde5bac46bc8e1f34af2feb8a
AdriaMayo <mayo.ortiz.adria@alumnat.copernic.cat>
2024-10-10 10:46:43 +0200

    Test datas i librerias del pom

--- src/main/java/dades/MyDataSource.java
+++ src/main/java/dades/MyDataSource.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -50,3 +51,4 @@ public class MyDataSource {
     }
     
 }
+>>>>>>> THEIRS
